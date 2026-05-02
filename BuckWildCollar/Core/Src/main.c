0f0fdfd9abee11dcbe7b7010ca9f3aa49dd03a5b
Paul <eickpb@gmail.com>
2026-05-02 01:48:51 -0500

    Working MAXM10S The receiver will take about 1-2 minutes to start up.

--- BuckWildCollar/Core/Src/main.c
+++ BuckWildCollar/Core/Src/main.c
@@ -20,7 +20,7 @@
 #include "tim.h"
 #include "gpio.h"
 
-#include "m_gps.h"
+
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -33,8 +33,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 64
+#define BUFFER_SIZE 128
 #define UART_DELAY 10
+#define UART_GPS_DELAY 1000
+
+// START GPS
+
+#define RX_LINE_MAX 128
+#define RX_QUEUE_LINES 16
+
+static uint8_t rx_byte;
+
+static char build_line[RX_LINE_MAX];
+static uint16_t build_idx = 0;
+
+static char line_q[RX_QUEUE_LINES][RX_LINE_MAX];
+static volatile uint8_t q_head = 0, q_tail = 0;
+
+static volatile uint32_t lines_ok = 0;
+static volatile uint32_t lines_drop = 0;
+
+
+typedef struct {
+    char utc[16];
+    float lat;
+    float lon;
+    int sats;
+    float hdop;
+    int valid;
+} gps_data_t;
+
+static gps_data_t gps;
+
+// END GPS
 
 typedef enum{
 	active,
@@ -94,6 +125,11 @@ uint8_t reset = 0;
 // Prototypes
 static void print(const char *fmt, ...);
 static int Get_ADC();
+
+static int queue_push(const char *s);
+static int queue_pop(char *out);
+static void parse_nmea(char *line);
+static float nmea_to_deg(const char *s);
 
 // Functions
 
@@ -196,7 +232,7 @@ void bno_write_units(){
 	 * Write a message to the BNO055
 	 * TODO: Entire Function (not necessary if we are writing 0x00 since that is reset)
 	 */
-	uint8_t test_message[64];
+	uint8_t test_message[BUFFER_SIZE];
 	test_message[0] = 170;
 	test_message[1] = 0x00;
 	test_message[2] = 0x3B;
@@ -284,6 +320,112 @@ void print_hex(uint8_t *buffer, uint8_t length){
 	}
 }
 
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+ {
+   if (huart->Instance == LPUART1)
+   {
+     // Grab received byte and
+     uint8_t b = rx_byte;
+
+     // Build a line until '\n' (NMEA typical end is "\r\n")
+     if (b == '\r' || b == '\n')
+     {
+         if (build_idx > 0)
+         {
+             build_line[build_idx] = '\0';
+
+             if (queue_push(build_line))
+                 lines_ok++;
+             else
+                 lines_drop++;
+
+             build_idx = 0;
+         }
+     }
+     else
+     {
+         if (build_idx < RX_LINE_MAX - 1)
+             build_line[build_idx++] = (char)b;
+         else
+         {
+             build_idx = 0;
+             lines_drop++;
+         }
+     }
+
+     // Re-arm next byte
+     HAL_UART_Receive_IT(&hlpuart1, &rx_byte, 1);
+   }
+ }
+
+ static int queue_push(const char *s)
+ {
+   uint8_t next = (uint8_t)((q_head + 1) % RX_QUEUE_LINES);
+   if (next == q_tail) return 0; // full
+
+   strncpy(line_q[q_head], s, RX_LINE_MAX);
+   line_q[q_head][RX_LINE_MAX - 1] = '\0';
+   q_head = next;
+   return 1;
+ }
+
+ static int queue_pop(char *out)
+ {
+   if (q_tail == q_head) return 0; // empty
+
+   strncpy(out, line_q[q_tail], RX_LINE_MAX);
+   out[RX_LINE_MAX - 1] = '\0';
+   q_tail = (uint8_t)((q_tail + 1) % RX_QUEUE_LINES);
+   return 1;
+ }
+
+ static float nmea_to_deg(const char *s)
+ {
+     if (!s || !*s) return 0.0f;
+
+     float v = (float)atof(s);
+     int deg = (int)(v / 100.0f);
+     float min = v - (deg * 100.0f);
+     return deg + (min / 60.0f);
+ }
+
+ static void parse_nmea(char *line)
+ {
+     char *f[20] = {0};
+     int n = 0;
+
+     char *tok = strtok(line, ",");
+     while (tok && n < 20) {
+         f[n++] = tok;
+         tok = strtok(NULL, ",");
+     }
+
+     if (n == 0) return;
+
+     if (!strncmp(f[0], "$GNRMC", 6) || !strncmp(f[0], "$GPRMC", 6))
+     {
+         if (n > 6) {
+             strncpy(gps.utc, f[1] ? f[1] : "", sizeof(gps.utc)-1);
+             gps.utc[sizeof(gps.utc)-1] = '\0';
+
+             gps.valid = (f[2] && f[2][0] == 'A');
+
+             gps.lat = nmea_to_deg(f[3]);
+             if (f[4] && f[4][0] == 'S') gps.lat = -gps.lat;
+
+             gps.lon = nmea_to_deg(f[5]);
+             if (f[6] && f[6][0] == 'W') gps.lon = -gps.lon;
+         }
+     }
+     else if (!strncmp(f[0], "$GNGGA", 6) || !strncmp(f[0], "$GPGGA", 6))
+     {
+         if (n > 8) {
+             gps.sats = f[7] ? atoi(f[7]) : 0;
+             gps.hdop = f[8] ? (float)atof(f[8]) : 0.0f;
+         }
+     }
+ }
 
 
 
@@ -476,8 +618,37 @@ int main(void)
 		  print("lX: %d, lY: %d, lZ: %d, eX: %d, eY: %d, eZ: %d, Temp: %d\r\n", imu.lin_y, imu.lin_x, imu.lin_z, imu.eul_x, imu.eul_y, imu.eul_z, imu.temperature); // NOTE: LINEAR VALUES XYZ ARE NOT CORRESPONDING TO WHAT IS PRINTED!!!
 
 	  }
+//	  else if (state == demo_maxm10s){
+//		  memcpy(storage_buffer, empty_buffer, BUFFER_SIZE);
+//		  HAL_UART_Receive(&hlpuart1, storage_buffer, BUFFER_SIZE, UART_GPS_DELAY);
+//		  HAL_UART_Transmit(&huart1, storage_buffer, BUFFER_SIZE, UART_DELAY);
+//		  print("\r\n");
+//	  }
 	  else if (state == demo_maxm10s){
+    	  HAL_UART_Receive_IT(&hlpuart1, &rx_byte, 1);
+	      char line[RX_LINE_MAX];
 
+	      while (queue_pop(line))
+	      {
+	          parse_nmea(line);
+
+	          char out[128];
+
+	          int lat_i = (int)(gps.lat * 1000000);
+	          int lon_i = (int)(gps.lon * 1000000);
+	          int hdop_i = (int)(gps.hdop * 100);
+
+	          int len = snprintf(out, sizeof(out),
+	              "UTC:%s LATi:%d LONi:%d SAT:%d HDOPi:%d VALID:%d\r\n",
+	              gps.utc, lat_i, lon_i, gps.sats, hdop_i, gps.valid);
+	//          int len = snprintf(out, sizeof(out),
+	//                             "UTC:%s LAT:%.6f LON:%.6f SAT:%d HDOP:%.2f VALID:%d\r\n",
+	//                             gps.utc, gps.lat, gps.lon, gps.sats, gps.hdop, gps.valid);
+
+	          HAL_UART_Transmit(&huart1, (uint8_t*)out, len, 100);
+	      }
+
+	      HAL_Delay(1);
 	  }
 	  else{
 		  BSP_LED_Toggle(LED_RED);
--- gps_program/Core/Src/stm32u5xx_it.c
+++ gps_program/Core/Src/stm32u5xx_it.c
@@ -55,8 +55,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern UART_HandleTypeDef hlpuart1;
 extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart2;
 extern TIM_HandleTypeDef htim17;
 
 /* USER CODE BEGIN EV */
@@ -216,17 +216,17 @@ void USART1_IRQHandler(void)
 }
 
 /**
-  * @brief This function handles USART2 global interrupt.
+  * @brief This function handles LPUART1 global interrupt.
   */
-void USART2_IRQHandler(void)
+void LPUART1_IRQHandler(void)
 {
-  /* USER CODE BEGIN USART2_IRQn 0 */
+  /* USER CODE BEGIN LPUART1_IRQn 0 */
 
-  /* USER CODE END USART2_IRQn 0 */
-  HAL_UART_IRQHandler(&huart2);
-  /* USER CODE BEGIN USART2_IRQn 1 */
+  /* USER CODE END LPUART1_IRQn 0 */
+  HAL_UART_IRQHandler(&hlpuart1);
+  /* USER CODE BEGIN LPUART1_IRQn 1 */
 
-  /* USER CODE END USART2_IRQn 1 */
+  /* USER CODE END LPUART1_IRQn 1 */
 }
 
 /**
